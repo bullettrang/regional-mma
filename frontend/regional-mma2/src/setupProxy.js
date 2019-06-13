73763718167a8c05bffd7a4b0814a7ad5991c75c
Brian <trangb@oregonstate.edu>
2019-06-13 13:47:27 -0700

    proxy

--- frontend/regional-mma2/src/setupProxy.js
+++ frontend/regional-mma2/src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('../node_modules/http-proxy-middleware');
  
 module.exports = function(app) {
-    app.use(proxy(['/states/'], { target: 'http://localhost:5000' }));
+    app.use(proxy(['/api/'], { target: 'http://localhost:5000' }));
 };
