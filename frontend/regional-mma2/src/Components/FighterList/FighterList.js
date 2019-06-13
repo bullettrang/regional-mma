aef66d09e9230a9fc61bfd935fa287f560039788
Brian <trangb@oregonstate.edu>
2019-06-12 21:09:42 -0700

    change grid position and styling of prospects

--- frontend/regional-mma2/src/Components/FighterList/FighterList.js
+++ frontend/regional-mma2/src/Components/FighterList/FighterList.js
@@ -3,8 +3,8 @@ import Loader from 'react-loader-spinner'
 import Prospects from './Prospects/Prospects';
 const FighterList = (props)=>{
     return(
-      <div className="Content">
-      <h1>Top {props.selected !==null ? props.selected: "Regional"} Fighters</h1>
+      <div className="Fighter_Content">
+        <h1>Top {props.selected !==null ? props.selected: "Regional"} Fighters</h1>
         {props.loading? <Loader type="ThreeDots" color="#somecolor" height={80} width={80} />:<Prospects fighters={props.fighters}/>}
     </div>
     )
