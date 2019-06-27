b046b35a3c3bd890e6e0121ebdc0392e0ca9ba76
Brian <trangb@oregonstate.edu>
2019-06-27 16:04:38 -0700

    removed modal

--- frontend/regional-mma2/src/Components/CustomMap/CustomMap.js
+++ frontend/regional-mma2/src/Components/CustomMap/CustomMap.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { feature } from "topojson-client";
-
-import Modal from '../UI/Modal';
 import JSONmap from "../../static/us-albers.json";
 
 import {
